5c6d72a7605338a0393737da41436e1f11abd798
bcmrcool <jamesbond824@yahoo.com>
2020-02-09 18:34:00 -0800

    minor saga changes

--- loan-app/server.js
+++ loan-app/server.js
@@ -7,18 +7,24 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function getData(){
-	return {result: 'success'}
+function calculateResponse(){
+	return new Promise((resolve, reject) => {
+		resolve("Success")
+	})
 }
 
-app.get('/api/getData', (req, res) => {
-  const myThing = getData()
-  res.send({ express: myThing });
+app.get('/api/getUsers', (req, res) => {
 });
 
 app.post('/api/sendApplicantData', (req, res) => {
-  console.log(req.body);
-  res.send({thing:'received'});
+   const getData = calculateResponse()
+   getData
+  .then(function (data) {
+  	res.send({ express: data });
+  })
+  .catch(function(error){
+  	res.send({ express: error})
+  })
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
